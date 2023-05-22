656d9f7177dda16d765fa94196e647aacd27c059
Michal Töpfer <michal.topfer@gmail.com>
2023-05-22 19:27:09 +0200

    Update layout and add 404 page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import "bootswatch/dist/lux/bootstrap.min.css";
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
+import Navigation from "@/components/navigation";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Michal Töpfer</title>
+        <meta name="description" content="Osobní stránka Michala Töpfera" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Navigation />
+      <main className="container-xl mb-4 mt-4">
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
