7c9a830187a62cdab6e223fc4859e5eccd0fe7dc
Michal Töpfer <michal.topfer@gmail.com>
2023-08-15 11:07:39 +0200

    Update teaching archive

--- pages/vyuka/index.tsx
+++ pages/vyuka/index.tsx
@@ -3,6 +3,25 @@ import { globSync } from 'glob';
 import _ from 'lodash';
 import Link from "next/link";
 
+enum Semester {
+  Winter, Summer
+}
+
+const Courses: { [courseCode: string]: { name: string, semester: Semester }} = {
+  "NPRG062": {
+    name: "Algoritmizace",
+    semester: Semester.Winter,
+  },
+  "NPRG030": { 
+    name: "Programování 1",
+    semester: Semester.Winter,
+  },
+  "NPRG031": { 
+    name: "Programování 2",
+    semester: Semester.Summer,
+  },
+}
+
 export const getStaticProps: GetStaticProps<{
   courses: _.Dictionary<string[]>;
 }> = () => {
@@ -22,28 +41,35 @@ export default function Teaching({
   </>);
 }
 
-// TODO: split ZS and LS (based on course codes)
-function Year(year: string, courses: string[]) {
-  courses.sort();
+function Year(year: string, yearCourses: string[]) {
+  yearCourses.sort();
+  const fullYear = `20${year.substring(0, 2)}/20${year.substring(2, 4)}`;
   return (<section key={year} id={year}>
-    <h2>20{year.substring(0, 2)}/20{year.substring(2, 4)}</h2>
+    <h2>{fullYear}</h2>
+    <h3 id={`{year}zs`}>ZS {fullYear}</h3>
     <ul className="list-unstyled ps-5 py-2">
-      {courses.map(Course)}
+      {yearCourses.filter(c => isInSemester(c, Semester.Winter)).map(Course)}
+    </ul>
+    <h3 id={`{year}ls`}>LS {fullYear}</h3>
+    <ul className="list-unstyled ps-5 py-2">
+      {yearCourses.filter(c => isInSemester(c, Semester.Summer)).map(Course)}
     </ul>
   </section>)
 }
 
-const CourseNames: { [courseCode: string]: string} = {
-  "NPRG062": "Algoritmizace",
-  "NPRG030": "Programování 1",
-  "NPRG031": "Programování 2",
+function isInSemester(yearCourse: string, semester: Semester) {
+  const course = yearCourse.split("/")[1];
+  if (!Courses.hasOwnProperty(course)) throw new Error(`Unknown course: ${course} (in ${yearCourse})`);
+
+  return Courses[course].semester === semester;
 }
 
 function Course(yearCourse: string) {
   const course = yearCourse.split("/")[1];
+  if (!Courses.hasOwnProperty(course)) throw new Error(`Unknown course: ${course} (in ${yearCourse})`);
 
   return <li key={yearCourse}>
     <Link className="d-inline-flex align-items-center rounded"
-          href={`/vyuka/${yearCourse}/`}>{course} &ndash; {CourseNames[course]}</Link>
+          href={`/vyuka/${yearCourse}/`}>{course} &ndash; {Courses[course].name}</Link>
   </li>;
 }
