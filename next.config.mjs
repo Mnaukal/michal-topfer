3db54c5c5098b2b50ed61c3f7e37b04d466d5e49
Michal Töpfer <michal.topfer@gmail.com>
2023-11-27 19:47:17 +0100

    Add support for MDX

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+import createMDX from '@next/mdx'
+import rehypeKatex from 'rehype-katex'
+import rehypeSlug from 'rehype-slug'
+import remarkMath from 'remark-math'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === "production" ? "/~topfermi" : "",
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],  // add MDX
+}
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex, rehypeSlug],
+  },
+})
+ 
+export default withMDX(nextConfig)
--- pages/vyuka/2324/NPRG030/index.tsx
+++ pages/vyuka/2324/NPRG030/index.tsx
@@ -42,7 +42,7 @@ export default function CourseIndex() {
       <p className="text-danger">Cvičení nebude z důvodu konání Dne otevřených dveří.</p>
     </Lab>,
     <Lab key="8" order="8" date="28.11." plan>
-      <p>Algoritmizace: zásobník, fronta, halda &ndash; <Link href="NPRG062/08#cyklicka-fronta-v-poli">cyklická fronta v poli</Link>, <Link href="NPRG062/08#operace-nad-haldou">operace nad haldou</Link>.</p>
+      <p>Algoritmizace: zásobník, fronta, halda &ndash; <Link href="NPRG062/08#cyklická-fronta-v-poli">cyklická fronta v poli</Link>, <Link href="NPRG062/08#operace-nad-haldou">operace nad haldou</Link>.</p>
       <p>Programování: podrobnosti k zápočtovým programům (též <a href="#zapoctak">níže</a>), <a href="https://git-scm.com/">Git</a> (<a href="https://pyvec.github.io/cheatsheets/basic-git/basic-git-cs.pdf">tahák</a>) a <a href="https://gitlab.mff.cuni.cz/">GitLab</a>, <Link href="NPRG030/08">dědičnost a kompozice</Link> (<LabPrograms source="lab" order="8"/>).</p>
     </Lab>,
     <Lab key="9" order="9" date="5.12." future>
