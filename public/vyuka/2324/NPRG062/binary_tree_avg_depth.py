3c495e9dbf304cfa7bcb0c4be14cd0460c76274a
Michal Töpfer <michal.topfer@gmail.com>
2023-12-12 16:37:21 +0100

    ZS 10

--- public/vyuka/2324/NPRG062/binary_tree_avg_depth.py
+++ public/vyuka/2324/NPRG062/binary_tree_avg_depth.py
@@ -1,4 +1,9 @@
-# Navrhněte efektivní algoritmus, která zjistí průměrnou výšku stromu definovanou jako `součet_délek_všech_cest_z_kořene_do_listu / počet_listů`. Délka cesty je počet hran.
+"""
+Pro binární strom navrhněte efektivní algoritmus, který zjistí průměrnou výšku stromu definovanou jako `součet_délek_všech_cest_z_kořene_do_listu / počet_listů`. Délka cesty je počet hran.
+
+
+Zajímá nás jen tvar stromu, ne hodnoty ve vrcholech. Strom v šabloně tvarem odpovídá příkladu ze zadání (jeho vrcholy jsou pro snazší ladění očíslované po hladinách odshora dolů).
+"""
 
 
 class Node:
@@ -12,14 +17,16 @@ class Node:
 
 
 def avg_depth(root):
-    # TODO: implement
+    # TODO: implementujte
     return 0
     
 
 if __name__ == "__main__":
+    # řádky jsou úmyslně zalomené takhle pro snazší orientaci
+    # vždy je Node(hodnota, levý_podstrom, pravý_podstrom)
     root = \
-        Node(1, 
-            Node(2, 
+        Node(1,
+            Node(2,
                 Node(4,
                     None,
                     Node(8)
@@ -29,11 +36,11 @@ if __name__ == "__main__":
                     None
                 )
             ),
-            Node(3, 
+            Node(3,
                 Node(6),
                 Node(7,
                     None,
-                    Node(10, 
+                    Node(10,
                         Node(11),
                         Node(12)
                     )
@@ -41,4 +48,4 @@ if __name__ == "__main__":
             )
         )
 
-    print(avg_depth(root))
+    print(avg_depth(root))  # mělo by vám vyjít 3.2
