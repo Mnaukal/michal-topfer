48415c0c1aae00f1c16194613ff0dfccb4e7fa9e
Michal Töpfer <michal.topfer@gmail.com>
2024-01-10 14:09:58 +0100

    ZS 12 po cvičení

--- public/vyuka/2324/NPRG062/cesta_kralem.py
+++ public/vyuka/2324/NPRG062/cesta_kralem.py
@@ -17,13 +17,13 @@ class ChessBoard:
         self.board_size = board_size
         self.board = [[NOT_VISITED for _col in range(board_size)] for _row in range(board_size)]
 
-    def add_obstacle(self, row, column):
+    def add_obstacle(self, row: int, column: int):
         self.board[row][column] = OBSTACLE
 
-    def get_distance(self, row, column):
+    def get_distance(self, row: int, column: int) -> int:
         return self.board[row][column]
 
-    def set_distance(self, row, column, distance):
+    def set_distance(self, row: int, column: int, distance: int):
         self.board[row][column] = distance
 
     def clear_distances(self):
@@ -33,8 +33,11 @@ class ChessBoard:
                 if self.get_distance(row, column) != OBSTACLE:
                     self.set_distance(row, column, NOT_VISITED)
         
-    def get_neigbors(self, row, column) -> list[tuple[int, int]] | Iterator[tuple[int, int]]:
-        """Returns a list (or generator) of neighbor cells (pairs of integers)."""
+    def get_neigbors(self, row: int, column: int) -> list[tuple[int, int]] | Iterator[tuple[int, int]]:
+        """
+        Returns a list (or generator) of neighbor cells (pairs of integers).
+        Note that the row and column coordinates are numbers in the range 0 to 7 (i.e., less than self.board_size).
+        """
         # TODO: vraťte možné pohyby krále (8 sousedních políček šachovnice)
         return [(-1, -1)]
         
@@ -46,19 +49,22 @@ class ChessBoard:
             print()
 
     def bfs(self, start_row, start_column):
+        """Use Breadth-first search to find the shortest path from start to target."""
         self.clear_distances()  # this is only necessary if `bfs` is run multiple times
 
-        queue = Queue()
+        queue: Queue[tuple[int, int]] = Queue()
+
         self.set_distance(start_row, start_column, 0)
         queue.put(...)  # TODO: přidejte startovní políčko do fronty
+        # pozor na to, že pokud chcete políčka šachovnice reprezentovat pomocí dvojic čísel (řádek, sloupec), tak nesmíte při přidávání do fronty zapomenout na závorky, např.: queue.put((start_row, start_column))
 
         while not queue.empty():
             ... = queue.get()  # TODO: načtěte políčko z fronty
-            distance = self.get_distance(...)  # TODO: přečtěte vzdálenost aktuálního políčka
+            distance = self.get_distance(..., ...)  # TODO: přečtěte vzdálenost aktuálního políčka
 
-            for neighbor_cell in self.get_neigbors(...):  # TODO: projděte všechny sousedy aktuálního políčka
+            for neighbor_row, neighbor_column in self.get_neigbors(..., ...):  # TODO: projděte všechny sousedy aktuálního políčka
                 # TODO: buď tady ověřte, že jste sousední políčko ještě nenavštívili, nebo z `get_neigbors` vracejte jen nenavštívené sousedy
-                self.set_distance(...)  # TODO: nastavte vzdálenost pro souseda
+                self.set_distance(..., ..., ...)  # TODO: nastavte vzdálenost pro souseda
                 queue.put(...)  # TODO: přidejte souseda do fronty
 
 
@@ -76,7 +82,7 @@ def main():
 
     chess_board.bfs(*start)
 
-    # chess_board.print()  # uncomment for debugging
+    # chess_board.print()  # uncomment for debugging (and don't forget to comment back out when submitting to ReCodEx)
 
     print(chess_board.get_distance(*target))  # distance to the target position
 
